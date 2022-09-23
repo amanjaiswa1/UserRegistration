93ff87c956fd9a67414083106406319296df71bb
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-23 23:31:59 +0530

    Add: Validate Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	static final String firstNamePattern = "^[A-Z][a-z]{2,}";
+	static final String lastNamePattern = "^[A-Z][a-z]{2,}";
 
 	// UseCase1 : Validate first name.
 	public static void validateFirstName(String firstName) {
@@ -17,9 +18,24 @@ public class UserRegistration {
 			System.out.println("Match Not Found.");
 	}
 
+	// UseCase2 : Validate last name.
+	public static void validateLastName(String lastName) {
+		Pattern pattern = Pattern.compile(lastNamePattern);
+		Matcher matcher = pattern.matcher(lastName);
+		boolean matchFound = matcher.find();
+		if (matchFound)
+			System.out.println("Match Found.");
+		else
+			System.out.println("Match Not Found.");
+	}
+
 	public static void main(String[] args) {
 		// UseCase1 : Validate first name.
 		String firstName = "Tekesh";
 		validateFirstName(firstName);
+		
+		// UseCase2 : Validate last name.
+		String lastName = "Singh";
+		validateLastName(lastName);
 	}
 }
