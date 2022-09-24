77501090ee6f099524a41540d7bb1338c18654de
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-24 10:39:54 +0530

    Add: Validate Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	static final String firstNamePattern = "^[A-Z][a-z]{2,}";
 	static final String lastNamePattern = "^[A-Z][a-z]{2,}";
+	static final String emailPattern = "^[a-z0-9]+[.]?[a-z0-9]*@[a-z0-9]+[.][a-z]+([.]?[a-z])*";
 
 	// UseCase1 : Validate first name.
 	public static void validateFirstName(String firstName) {
@@ -29,13 +30,28 @@ public class UserRegistration {
 			System.out.println("Match Not Found.");
 	}
 
+	// UseCase3 : Validate email.
+	public static void validateEmail(String email) {
+		Pattern pattern = Pattern.compile(emailPattern);
+		Matcher matcher = pattern.matcher(email);
+		boolean matchFound = matcher.find();
+		if (matchFound)
+			System.out.println("Match Found.");
+		else
+			System.out.println("Match Not Found.");
+	}
+
 	public static void main(String[] args) {
 		// UseCase1 : Validate first name.
 		String firstName = "Tekesh";
 		validateFirstName(firstName);
-		
+
 		// UseCase2 : Validate last name.
 		String lastName = "Singh";
 		validateLastName(lastName);
+
+		// UseCase3 : Validate email.
+		String email = "tekesh.singhgmail.co.in";
+		validateEmail(email);
 	}
 }
